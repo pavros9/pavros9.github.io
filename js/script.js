464e08b74d310d87a283d6715ed8d0748b63f160
pavros9 <roman.pavlov.1996@bk.ru>
2021-05-05 23:37:11 +0300

    Ver_1

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.section-header-burger').click(function(event){
+        $('.section-inner-header-for_menu,.section-header-burger, .section-inner-header').toggleClass('active');
+    });
+});
