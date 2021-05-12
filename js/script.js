53d679dd82f2a3c57212b4d855801250731159e8
pavros9 <roman.pavlov.1996@bk.ru>
2021-05-12 22:17:33 +0300

    Ver_12

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
-    $('.section-header-burger').click(function (event) {
-        $('.section-inner-header-for_menu,.section-header-burger, .section-inner-header').toggleClass('active');
-    });  
-});
+  $('.section-header-burger').click(function (event) {
+    $('.section-header-burger, .section-header').toggleClass('active');
+  });
 
+  function resizeBlock() {
+    if (window.matchMedia("screen and (max-width: 1919px)").matches) {
+      $('.section-short_description-outer').appendTo($('.section-short_description-right'));
+    } else if (window.matchMedia("screen and (min-width: 1920px)").matches) {
+      $('.section-short_description-outer').appendTo($('.section-short_description'));
+    }
+  }
+
+  $(window).resize(function () {
+    resizeBlock();
+  });
+
+  $(document).ready(function () {
+    resizeBlock();
+  });
+
+
+});
