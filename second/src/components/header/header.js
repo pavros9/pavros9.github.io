19e9cdaf256530c31518412ff8d8ead200808a67
pavros9 <roman.pavlov.1996@bk.ru>
2022-01-16 17:25:10 +0300

    "header" was fixed

--- second/src/components/header/header.js
+++ second/src/components/header/header.js
@@ -5,7 +5,7 @@ const headerWrapperBurger = document.querySelector(".header__wrapper-burger");
 const headerNavigation = document.querySelector(".header__navigation");
 
 headerWrapperBurger.onclick = function () {
-  headerNavigation.classList.toggle("header__navigation-open");
-  headerBurger.classList.toggle("active");
-  document.body.classList.toggle("lock");
+  headerNavigation.classList.toggle("header__navigation_open");
+  headerBurger.classList.toggle("header__burger_active");
+  document.body.classList.toggle("body_lock");
 };
